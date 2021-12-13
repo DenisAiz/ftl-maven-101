ef1654956906aa4812f042bd8702104dd27cf475
DenAiz <git@github.com:DenisAiz/ftl-jb-001.git>
2021-12-13 15:05:30 +0300

    MVN001 - init project

---
+++ service/src/main/java/com/denaiz/maven/service/PersonService.java
@@ -0,0 +1,19 @@
+package com.denaiz.maven.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.denaiz.maven.core.Person;
+
+public class PersonService {
+    public Person parse(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person person = objectMapper.readValue(json, Person.class);
+        return person;
+    }
+
+    public String convert(Person person) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(person);
+    }
+
+}
